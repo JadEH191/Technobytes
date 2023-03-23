62904fe76ccc69bf0c9630d3dc98f8899c52dfa1
UNIT012 <forpersonalonly191@gmail.com>
2023-03-23 14:34:42 +0800

    test

--- app/src/main/java/com/example/technobytes/Shop.java
+++ app/src/main/java/com/example/technobytes/Shop.java
@@ -5,8 +5,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -14,12 +17,15 @@ import java.util.List;
 public class Shop extends AppCompatActivity {
 
     RecyclerView rvProduct;
+
+    Button btnToCart;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_shop);
 
         rvProduct = findViewById(R.id.rvProduct);
+        btnToCart = findViewById(R.id.btnToCart);
 
         List<Items> items = new ArrayList<Items>();
 
@@ -46,5 +52,17 @@ public class Shop extends AppCompatActivity {
 
         rvProduct.setLayoutManager(new LinearLayoutManager(this));
         rvProduct.setAdapter(new Adapter(getApplicationContext(), items));
+
+        btnToCart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                toMyCart();
+            }
+        });
+    }
+
+    public void toMyCart() {
+        Intent intent = new Intent(this, Cart.class);
+        startActivity(intent);
     }
 }
